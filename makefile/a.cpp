4e85144e6526032ff4e1a64d42be14a9f94c40c6
ylqfp <ylqfps@gmail.com>
2015-08-25 12:08:30 +0800

    makefile learning

---
+++ makefile/a.cpp
@@ -0,0 +1,11 @@
+#include "a.h"
+
+int a(){
+	std::cout<<"In a.cpp"<<std::endl;
+}
+
+/*
+int main(){
+	a();
+}
+*/
---
+++ makefile/b.cpp
@@ -0,0 +1,6 @@
+#include "b.h"
+
+int b(){
+	std::cout<<"In b.cpp"<<std::endl;
+
+}
